c54537b91e9edd9170d273f1eb84bf2478f5cd0b
Ryan <ry762981@dal.ca>
2023-02-01 15:03:42 -0400

    Fix merge conflicts

--- backend/server/server.js
+++ backend/server/server.js
@@ -2,6 +2,24 @@
 const express = require('express');
 const app = express();
 
+// Connect to MySQL
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    //port: '3306',
+    user: 'root',
+    password: '',
+    database: 'green_card'
+  });
+  connection.query(
+    'SELECT * FROM example_table',
+    function(error, results, fields) {
+      if (error) throw error;
+      console.log(results);
+    }
+  );
+  connection.end();
+
 app.get('/api/users', (req, res) => {
     const users = [{ name: 'John', age: 30 }, { name: 'Samantha', age: 25 }];
     res.json(users);
