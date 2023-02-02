f72b71d01385aecdc388c3df72ad1900c859318d
Mat <matingh_si@hotmail.com>
2023-02-01 20:09:10 -0400

    front-end login page added working in react, it needs some work in back-end to be fully functional and submit user password

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoginIndex from './views/Login/LoginIndex';
+import { BrowserRouter, Routes ,Route }from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App/>} />
+      <Route path="/login" element={<LoginIndex/>}/>
+    </Routes>
+  </BrowserRouter>
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ my-app/src/views/Login/LoginIndex.js
@@ -0,0 +1,123 @@
+// Citations:
+// Title: Sign-in template
+// Author: Michal Dudak and Samuel Sycamore
+// Date: Oct 27, 2022
+// Date accessed: Feb 01, 2023
+// Code version: 3f88e94
+// Availability: https://github.com/mui/material-ui/tree/v5.11.7/docs/data/material/getting-started/templates/sign-in
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+// This function returns a Typography component with text that displays a copyright notice for the website. It includes a link to the website and the current year. The props argument allows for additional properties to be passed into the Typography component.
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+// This code is a function called SignIn() that renders a form with an email and password field, and a submit button. When the submit button is clicked, the handleSubmit() function is called which prevents the default action from occuring, creates a FormData object from the currentTarget of the event, and logs an object containing the email and password values to the console.
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+// This code is a React component that renders a sign in form. It uses the ThemeProvider component to set the theme, and the Container component to set the maxWidth of the page. The CssBaseline component is used to reset any global styles. 
+// The Box component is used to display flex elements, and an Avatar component with a LockOutlinedIcon is used for the avatar. A Typography component is used for the title "Sign In". 
+// A TextField component is used for both email and password inputs, as well as a FormControlLabel with a Checkbox for "Remember me". A Button component is used for submitting the form, and two Grid components are used with Link components for "Forgot Password" and "Don't have an account? Sign Up". Finally, a Copyright component is rendered at the bottom of the page.
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
+  );
+}
