1d9b9af3fdc7f700d5c3f5a80ca910359eff6d4e
Ryan <ry762981@dal.ca>
2023-02-23 17:34:14 -0400

    Removing unused warnings to attempt to fix pipeline

--- my-app/src/views/Topics/MoodStabilizersGuide.js
+++ my-app/src/views/Topics/MoodStabilizersGuide.js
@@ -6,22 +6,14 @@
 // Code version: 3f88e94
 // Availability: https://github.com/mui/material-ui/tree/v5.11.7/docs/data/material/getting-started/templates/sign-in
 import * as React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 
