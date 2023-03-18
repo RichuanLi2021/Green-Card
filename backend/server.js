4dfbaeef9748206d4903579b7ef0f38561e16159
Mat <mt662270@dal.ca>
2023-03-18 00:29:16 -0300

    building back-end

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const app = express();
+"use strict";
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your React app's URL
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
-
-const routes = require('./routes/loginRoute');
+var express = require('express');
+var app = express();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your React app's URL
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+var routes = require('./routes/loginRoute');
 
 //for parsing application/json
 app.use(express.json());
 app.use('/api', routes);
-
-const port = 8887;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+var port = 8887;
+app.listen(port, function () {
+  console.log("Server started on port ".concat(port));
 });
