0aae556499af75fe9ff94b6b9b16f116a473dc33
Ryan <ry762981@dal.ca>
2023-03-28 21:54:44 -0300

    removed console log from testing

--- backend/controller/feedbackController.js
+++ backend/controller/feedbackController.js
@@ -3,7 +3,6 @@ const pool = require('../config/database');
 const postFeedback = {
     postFeedback: async (req, res, next) => {
         const { name, email, comment, rating, subscribe } = req.body;
-        console.log(name, email, comment);
         try {
             await pool.query('INSERT INTO feedback (name, email, comment, overall_rating, subscribe) VALUES (?, ?, ?, ?, ?)',
                 [name, email, comment, rating, subscribe]);
