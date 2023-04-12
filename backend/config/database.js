75663fb07acfaaf89e15ff0c5341c71ff5532a41
Ryan <Ryan-Fischback@hotmail.com>
2023-04-12 16:41:21 -0300

    setting up for deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
 const dbConfig = require('../config/databaseConfig.json');
 
-const {host, user, password, database} = dbConfig.database;
+const {host, user, password, database, port} = dbConfig.database;
 
 const pool = mysql.createPool({
   host: host,
   user: user,
   password: password,
   database: database,
+  port: port,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
--- frontend/src/views/Topics/NeuropsychiatricSymptoms/NeuropsychiatricSymptoms.js
+++ frontend/src/views/Topics/NeuropsychiatricSymptoms/NeuropsychiatricSymptoms.js
@@ -47,7 +47,7 @@ export default function NeuropsychiatricSymptoms() {
     
   const [data, setData] = useState([]);
   useEffect(() => {
-    axios.get('http://localhost:8887/api/neuropsychiatricSymptoms')
+    axios.get('https://gpgc-service.onrender.com/api/neuropsychiatricSymptoms')
         .then(response => {
           setData(response.data)
           console.log(response.data[0]);
