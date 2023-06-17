0734c5c0b7a997961fcb76d71dd0d983212488f2
Syed Sameer Hasan <sy952280@dal.ca>
2023-06-17 17:07:01 -0300

    updated moodstabilizer controller

--- backend/controller/MoodStabilizersController.js
+++ backend/controller/MoodStabilizersController.js
@@ -1,16 +1,44 @@
-const pool = require('../config/database');
+const pool = require("../config/database");
 
 const getMoodStabilizersControllerData = async () => {
-  const [rows, fields] = await pool.query('SELECT * FROM `green_card`.`mood stabilizers guide`');
+  const [rows, fields] = await pool.query(
+    "SELECT * FROM `green_card`.`mood stabilizers guide`"
+  );
   return rows;
 };
 
 const MoodStabilizersController = {
-   getData: async (req,res, next) => {
-    const MoodStabilizersControllerData = await getMoodStabilizersControllerData();
+  getData: async (req, res, next) => {
+    const MoodStabilizersControllerData =
+      await getMoodStabilizersControllerData();
     res.send(MoodStabilizersControllerData);
-   }
-
+  },
 };
 
-module.exports = MoodStabilizersController;
+const updateMoodStabilzerControllerData = async (req, res, next) => {
+  try {
+    const { name, column, value } = req.body;
+    await pool.query(
+      "UPDATE `green_card`.`mood stabilizers guide` SET " +
+        column +
+        " = " +
+        '"' +
+        value +
+        '"' +
+        " WHERE name = " +
+        '"' +
+        name +
+        '"'
+    );
+    res.send("Updated Successfully!");
+  } catch (error) {
+    console.log(error);
+    next(error);
+    throw error;
+  }
+};
+
+module.exports = {
+  updateMoodStabilzerControllerData,
+  MoodStabilizersController,
+};
