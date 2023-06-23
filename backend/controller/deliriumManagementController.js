e140f9ddd3d05c7a0521e9761a142d0737e1ebcf
Syed Sameer Hasan <sy952280@dal.ca>
2023-06-23 11:52:35 -0300

    delirium management controller implemented

---
+++ backend/controller/deliriumManagementController.js
@@ -0,0 +1,15 @@
+const pool = require("../config/database");
+
+const getDeliriumManagementControllerData = async () => {
+  const [rows, fields] = await pool.query("SELECT * FROM `green_card`.`DELIRIUM_MANAGMENT`");
+  return rows;
+};
+
+const deliriumManagementController = {
+  getData: async (req, res, next) => {
+    const deliriumManagementData = await getDeliriumManagementControllerData();
+    res.send(deliriumManagementData);
+  },
+};
+
+module.exports = deliriumManagementController;
