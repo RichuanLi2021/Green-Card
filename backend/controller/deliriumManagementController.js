ab6bc4e57dbe52fdf8cbd07e456558882263282d
Syed Sameer Hasan <sy952280@dal.ca>
2023-06-23 12:34:32 -0300

    delirium management router, server config implemented and bugs fixed

--- backend/controller/deliriumManagementController.js
+++ backend/controller/deliriumManagementController.js
@@ -1,7 +1,7 @@
 const pool = require("../config/database");
 
 const getDeliriumManagementControllerData = async () => {
-  const [rows, fields] = await pool.query("SELECT * FROM `green_card`.`DELIRIUM_MANAGMENT`");
+  const [rows, fields] = await pool.query("SELECT * FROM `green_card`.`DELIRIUM_MANAGEMENT`");
   return rows;
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,43 @@
 "use strict";
-var express = require('express');
+var express = require("express");
 var app = express();
-var cors = require('cors')
+var cors = require("cors");
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your React app's URL
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'DELETE, POST, GET, OPTIONS');
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // Replace with your React app's URL
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
   next();
 });
 
-
-var routeLogin = require('./routes/loginRoute');
-var routeFeedback = require('./routes/feedbackRoute');
-var apRoute = require('./routes/antipsychoticsGuideRoute');
-var cogERoute = require('./routes/cogEnhancerRoute');
-var neuropsychiatricSymptomsRoute = require('./routes/neuropsychiatricSymptomsRoute');
-var InsomniaSedativesGuideRoute = require('./routes/InsomniaSedativesGuideRoute');
-var insomniaDeprescribingRoute = require('./routes/insomniaDeprescribingRoute');
-var InsomniaClinicalRoute = require('./routes/InsomniaClinicalRoute');
-var InsomniaSafetyRoute = require('./routes/InsomniaSafetyRoute');
-var MoodStabilizersRoute = require('./routes/MoodStabilizersRoute');
-var PsychotropicMonitoringSectionRoute = require('./routes/PsychotropicMonitoringSectionRoute');
-
-
+var routeLogin = require("./routes/loginRoute");
+var routeFeedback = require("./routes/feedbackRoute");
+var apRoute = require("./routes/antipsychoticsGuideRoute");
+var cogERoute = require("./routes/cogEnhancerRoute");
+var neuropsychiatricSymptomsRoute = require("./routes/neuropsychiatricSymptomsRoute");
+var InsomniaSedativesGuideRoute = require("./routes/InsomniaSedativesGuideRoute");
+var insomniaDeprescribingRoute = require("./routes/insomniaDeprescribingRoute");
+var InsomniaClinicalRoute = require("./routes/InsomniaClinicalRoute");
+var InsomniaSafetyRoute = require("./routes/InsomniaSafetyRoute");
+var deliriumManagementRoute = require("./routes/deliriumManagementRoute");
+var MoodStabilizersRoute = require("./routes/MoodStabilizersRoute");
+var PsychotropicMonitoringSectionRoute = require("./routes/PsychotropicMonitoringSectionRoute");
 
 //for parsing application/json
 app.use(express.json());
-app.use(cors({origin: true}))
-app.use('/api', routeLogin);
-app.use('/api', routeFeedback);
-app.use('/api', apRoute);
-app.use('/api/', cogERoute);
-app.use('/api', neuropsychiatricSymptomsRoute);
-app.use('/api', InsomniaSedativesGuideRoute);
-app.use('/api', insomniaDeprescribingRoute);
-app.use('/api', InsomniaClinicalRoute);
-app.use('/api', InsomniaSafetyRoute);
-app.use('/api', MoodStabilizersRoute);
-app.use('/api', PsychotropicMonitoringSectionRoute);
+app.use(cors({ origin: true }));
+app.use("/api", routeLogin);
+app.use("/api", routeFeedback);
+app.use("/api", apRoute);
+app.use("/api/", cogERoute);
+app.use("/api", neuropsychiatricSymptomsRoute);
+app.use("/api", InsomniaSedativesGuideRoute);
+app.use("/api", insomniaDeprescribingRoute);
+app.use("/api", InsomniaClinicalRoute);
+app.use("/api", InsomniaSafetyRoute);
+app.use("/api", deliriumManagementRoute);
+app.use("/api", MoodStabilizersRoute);
+app.use("/api", PsychotropicMonitoringSectionRoute);
 
 var port = 8887;
 app.listen(port, function () {
