655cc420901c3d429c98f64c411ab0563e8a35b0
Omar Dahleh <om885308@dal.ca>
2023-06-28 11:59:39 -0300

    logout button fixedwhen user is logged in, desktop view

--- backend/routes/antipsychoticsGuideRoute.js
+++ backend/routes/antipsychoticsGuideRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const apController = require ('../controller/antipsychoticsGuideController');
+const apController = require('../controller/antipsychoticsGuideController');
 
 router.get('/antipsychoticsGuide', apController.getData);
 router.post('/antipsychoticsGuide/update', apController.updateData);
 
 module.exports = router
+
