a7eb1a12c96fa878191ec1c8b693292001e82552
kulween <kl544366@dal.ca>
2023-07-03 23:42:42 -0300

    Changed tables under insomnia sedatives to button layouts for a better mobile view

--- frontend/src/views/Topics/InsomniaSedatives/InsomniaSedatives.js
+++ frontend/src/views/Topics/InsomniaSedatives/InsomniaSedatives.js
@@ -4,18 +4,10 @@ import Typography from '@mui/material/Typography';
 import axios from 'axios';
 import {useState, useEffect} from 'react';
 import SearchBar from "../../searchBar/searchBar";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+
 
 import Navigation from '../../Navigation/navigation';
-import Footer from '../../Footer/Footer';
+
 
 import Data from "../../searchBar/Data.json";
 
