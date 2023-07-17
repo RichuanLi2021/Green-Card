78df225d21699face6ebf32ab5a5f6e0f58ac678
Anu Tanna <anutanna@tf16d.wpa.dal.ca>
2023-07-17 11:39:20 -0300

    add/delete MS

--- frontend/src/views/Topics/MoodStabilizers/moodStabilizersbackend.js
+++ frontend/src/views/Topics/MoodStabilizers/moodStabilizersbackend.js
@@ -17,5 +17,22 @@ const moodStabilizersUpdate = async (name, column, value) => {
     throw error; // throw error to be handled by calling function
   }
 };
+export const submitDrug = async (doseInitial, frequency, mgFormSupplied, halfLife, monitoringLevel) => {
+  try {
+    const response = await axios.post('http://localhost:8887/api/add/MoodStabilizers', {
+      doseInitial,
+      frequency, 
+      mgFormSupplied, 
+        halfLife,
+        monitoringLevel, 
+    
+    });
+    console.log(response.data); // log response from server
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error; // throw error to be handled by calling function
+  }
+};
 
 export default moodStabilizersUpdate;
