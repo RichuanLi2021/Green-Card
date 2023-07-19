58876905f4d80319131f23c20e2e8feb9a6a3228
Aniket <an469681@dal.ca>
2023-07-19 10:24:35 -0300

    anticholinergic- new table structure

--- backend/controller/deliriumController.js
+++ backend/controller/deliriumController.js
@@ -1,16 +1,10 @@
 const pool = require('../config/database');
 
 const getAPData = async () => {
-    const [rows, fields] = await pool.query('SELECT * FROM `green_card`.`delirium_management`');
+    const [rows, fields] = await pool.query('SELECT * FROM `green_card`.`ANTICHOLINERGIC ACTIVITY`');
     return rows;
   };
-  
-  {/*const DeliriumController = {
-     getData: async (req,res, next) => {
-        const deliriumData = await  getDeliriumControllerData();
-    res.send(deliriumData);
-   }
-};*/}
+
 
 const getData = async (req, res, next) => {
   const apData = await getAPData();
@@ -20,7 +14,7 @@ const getData = async (req, res, next) => {
 const updateData = async (req, res, next) => {
   try {
     const { name, column, value } = req.body;
-    await pool.query('UPDATE `green_card`.`delirium_management` SET ' + column + ' = ' + '"' + value + '"' + ' WHERE High = ' + '"' + name + '"');
+    await pool.query('UPDATE `green_card`.`ANTICHOLINERGIC ACTIVITY` SET ' + column + ' = ' + '"' + value + '"' + ' WHERE Description = ' + '"' + name + '"');
     res.send("Updated Successfully!");
   } catch (error) {
     console.log(error);
