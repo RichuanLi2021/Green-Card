7cedc8ce375f978d25ef5e70f50833995a99b7b4
Syed Sameer Hasan <sy952280@dal.ca>
2023-07-20 23:49:40 -0300

    organized and grouped fetched data in controller

--- backend/controller/CognitiveEnhancersClinicalController.js
+++ backend/controller/CognitiveEnhancersClinicalController.js
@@ -1,42 +1,58 @@
-const pool = require('../config/database');
-
+const pool = require("../config/database");
 
 const getAPData = async () => {
-  const [rows, fields] = await pool.query('SELECT * FROM `green_card`.`COGNITIVE ENHANCERS CLINICAL GUIDE`');
+  const [rows, fields] = await pool.query(
+    "SELECT * FROM `green_card`.`COGNITIVE ENHANCERS CLINICAL GUIDE` ORDER BY `LIST_HEADERS_Id`"
+  );
   return rows;
 };
 
 const getData = async (req, res, next) => {
   const apData = await getAPData();
-  res.send(apData);
-}
 
+  const groupedData = apData.reduce((accumulator, currentValue) => {
+    if (!accumulator[currentValue.LIST_HEADERS_Id]) {
+      accumulator[currentValue.LIST_HEADERS_Id] = [];
+    }
+    accumulator[currentValue.LIST_HEADERS_Id].push(currentValue.Description);
+    return accumulator;
+  }, {});
+
+  res.send(groupedData);
+};
 
 const updateData = async (req, res, next) => {
   try {
     const { name, column, value } = req.body;
-    await pool.query('UPDATE `green_card`.`COGNITIVE ENHANCERS CLINICAL GUIDE` SET ' + column + ' = ' + '"' + value + '"' + ' WHERE Description = ' + '"' + name + '"');
+    await pool.query(
+      "UPDATE `green_card`.`COGNITIVE ENHANCERS CLINICAL GUIDE` SET " +
+        column +
+        " = " +
+        '"' +
+        value +
+        '"' +
+        " WHERE Description = " +
+        '"' +
+        name +
+        '"'
+    );
     res.send("Updated Successfully!");
   } catch (error) {
     console.log(error);
     next(error);
     throw error;
   }
-}
+};
 const drugData = async (req, res, next) => {
-  const {
-    listHeader, description
-  } = req.body;
-  
+  const { listHeader, description } = req.body;
+
   try {
     await pool.query(
       "INSERT INTO `green_card`.`COGNITIVE ENHANCERS CLINICAL GUIDE` (`LIST_HEADERS_Id`,`Description`) VALUES (?, ?)",
-      [
-        listHeader, description
-      ]
+      [listHeader, description]
     );
-    
-    res.send('Drug was submitted successfully');
+
+    res.send("Drug was submitted successfully");
   } catch (err) {
     next(err);
     throw err;
@@ -46,11 +62,10 @@ const drugData = async (req, res, next) => {
 const drugDelete = async (req, res, next) => {
   const Description = req.params.Description;
   try {
-    await pool.query(
-      "DELETE FROM `green_card`.`COGNITIVE ENHANCERS CLINICAL GUIDE` WHERE `Description` = ?",
-      [Description] 
-    );
-    res.send('Drug was deleted successfully');
+    await pool.query("DELETE FROM `green_card`.`COGNITIVE ENHANCERS CLINICAL GUIDE` WHERE `Description` = ?", [
+      Description,
+    ]);
+    res.send("Drug was deleted successfully");
   } catch (err) {
     next(err);
     throw err;
@@ -61,8 +76,5 @@ module.exports = {
   getData,
   updateData,
   drugData,
-  drugDelete
+  drugDelete,
 };
-
-
-
