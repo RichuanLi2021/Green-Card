5e34461b820e917d8329a466e8c0557d6b9829b5
Syed Sameer Hasan <sy952280@dal.ca>
2023-07-21 01:34:08 -0300

    admin view simple implementation

--- frontend/src/views/Topics/CognitiveEnhancersClinical/CognitiveEnhancersClinical.js
+++ frontend/src/views/Topics/CognitiveEnhancersClinical/CognitiveEnhancersClinical.js
@@ -135,173 +135,169 @@ export default function AntidepressantsClinical() {
   const hasData = Object.values(data).some((arr) => arr.length > 0);
 
   if (hasData) {
-    if (admin) {
-      return (
-        <>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
-          />
-          <Navigation />
-          <SearchBar placeholder="Search" data={Data} />
-          <div style={{ marginTop: "2rem", padding: "0 1rem" }}>
-            <Typography variant="h3" align="center" gutterBottom>
-              <div className="subtitle">Cognitive Enhancers Clinical Guide</div>
-            </Typography>
+    // if (admin) {
+    //   return (
+    //     <>
+    //       <link
+    //         rel="stylesheet"
+    //         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+    //       />
+    //       <Navigation />
+    //       <SearchBar placeholder="Search" data={Data} />
+    //       <div style={{ marginTop: "2rem", padding: "0 1rem" }}>
+    //         <Typography variant="h3" align="center" gutterBottom>
+    //           <div className="subtitle">Cognitive Enhancers Clinical Guide</div>
+    //         </Typography>
 
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 700 }} aria-label="customized table" id="antidepressantClinicalTable">
-                <TableHead>
-                  <TableRow>
-                    <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>ID</StyledTableCell>
-                    <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Description</StyledTableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.map((dataObj, index) => (
+    //         <TableContainer component={Paper}>
+    //           <Table sx={{ minWidth: 700 }} aria-label="customized table" id="antidepressantClinicalTable">
+    //             <TableHead>
+    //               <TableRow>
+    //                 <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>ID</StyledTableCell>
+    //                 <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Description</StyledTableCell>
+    //               </TableRow>
+    //             </TableHead>
+    //             <TableBody>
+    //               {data.map((dataObj, index) => (
+    //                 <StyledTableRow key={index}>
+    //                   <StyledTableCell component="th" scope="row">
+    //                     {dataObj.LIST_HEADERS_Id}
+    //                   </StyledTableCell>
+    //                   <StyledTableCell align="left">
+    //                     <input
+    //                       id="`Description`"
+    //                       name={dataObj.Description}
+    //                       type="text"
+    //                       onFocus={store_value}
+    //                       onBlur={update_value}
+    //                       defaultValue={dataObj[`Description`]}
+    //                     />
+    //                     <button
+    //                       style={{ background: "none", border: "none", cursor: "pointer" }}
+    //                       onClick={(e) => handleDelete(dataObj.Description)}
+    //                     >
+    //                       <span class="material-symbols-outlined">delete</span>
+    //                     </button>
+    //                   </StyledTableCell>
+    //                 </StyledTableRow>
+    //               ))}
+    //             </TableBody>
+    //           </Table>
+    //           <div className="box-content">
+    //             <div className="form-header">
+    //               <Box display="flex" justifyContent="space-between" alignItems="center">
+    //                 <Typography variant="h5" className="title">
+    //                   Add New Information to the page
+    //                 </Typography>
+    //               </Box>
+    //             </div>
+
+    //             <form onSubmit={handleSubmit}>
+    //               <Box>
+    //                 <TextField
+    //                   style={{ width: "400px" }}
+    //                   label="List Header (must be from one of above headers): "
+    //                   variant="filled"
+    //                   value={listHeader}
+    //                   onChange={handleHeader}
+    //                   multiline
+    //                   required
+    //                 />
+    //               </Box>
+
+    //               <Box>
+    //                 <TextField
+    //                   style={{ width: "400px" }}
+    //                   label="Description:"
+    //                   variant="filled"
+    //                   value={description}
+    //                   onChange={handleDescription}
+    //                   multiline
+    //                   required
+    //                 />
+    //               </Box>
+    //               <Box sx={{ display: "flex" }}>
+    //                 <Button
+    //                   style={{ width: "400px" }}
+    //                   type="submit"
+    //                   variant="contained"
+    //                   className="submit-button"
+    //                   color="primary"
+    //                 >
+    //                   Submit
+    //                 </Button>
+    //               </Box>
+    //             </form>
+    //           </div>
+    //         </TableContainer>
+    //         <br></br>
+    //       </div>
+    //       <div className="cognitive-footer">
+    //         <p className="cognitive-notes">
+    //           <b>Key: </b>COG_CONTRA means "Contraindications", COG_ACHEI means "Adverse Effects (AChEI)", COG_ACHEI_ME
+    //           means "Adverse Effects (Memantine)", COG_BASELINE means "Baseline", COG_MONITOR means "monitoring"
+    //         </p>
+    //       </div>
+    //       <Footer />
+    //     </>
+    //   );
+    // } else {
+    return (
+      <>
+        <Navigation />
+        <SearchBar placeholder="Search" data={Data} />
+        <div style={{ marginTop: "2rem", padding: "0 1rem" }}>
+          <Typography variant="h3" align="center" gutterBottom>
+            <div className="subtitle">Cognitive Enhancers Clinical Guide</div>
+          </Typography>
+
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table" id="antidepressantClinicalTable">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Contraindications</StyledTableCell>
+                  <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Adverse Effects (AChEI)</StyledTableCell>
+                  <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Adverse Effects (Memantine)</StyledTableCell>
+                  <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Baseline</StyledTableCell>
+                  <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Monitoring</StyledTableCell>
+                  <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>How & When</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Array(
+                  Math.max(
+                    data.COG_ACHEI.length,
+                    data.COG_ACHEI_ME.length,
+                    data.COG_BASELINE.length,
+                    data.COG_CONTRA.length,
+                    data.COG_MONITOR.length
+                  )
+                )
+                  .fill(null)
+                  .map((_, index) => (
                     <StyledTableRow key={index}>
-                      <StyledTableCell component="th" scope="row">
-                        {dataObj.LIST_HEADERS_Id}
-                      </StyledTableCell>
-                      <StyledTableCell align="left">
-                        <input
-                          id="`Description`"
-                          name={dataObj.Description}
-                          type="text"
-                          onFocus={store_value}
-                          onBlur={update_value}
-                          defaultValue={dataObj[`Description`]}
-                        />
-                        <button
-                          style={{ background: "none", border: "none", cursor: "pointer" }}
-                          onClick={(e) => handleDelete(dataObj.Description)}
-                        >
-                          <span class="material-symbols-outlined">delete</span>
-                        </button>
-                      </StyledTableCell>
+                      <StyledTableCell>{data.COG_CONTRA[index] || ""}</StyledTableCell>
+                      <StyledTableCell>{data.COG_ACHEI[index] || ""}</StyledTableCell>
+                      <StyledTableCell>{data.COG_ACHEI_ME[index] || ""}</StyledTableCell>
+                      <StyledTableCell>{data.COG_BASELINE[index] || ""}</StyledTableCell>
+                      <StyledTableCell>{data.COG_MONITOR[index] || ""}</StyledTableCell>
+                      <StyledTableCell>{(data.COG_HOW_WHEN && data.COG_HOW_WHEN[index]) || ""}</StyledTableCell>
                     </StyledTableRow>
                   ))}
-                </TableBody>
-              </Table>
-              <div className="box-content">
-                <div className="form-header">
-                  <Box display="flex" justifyContent="space-between" alignItems="center">
-                    <Typography variant="h5" className="title">
-                      Add New Information to the page
-                    </Typography>
-                  </Box>
-                </div>
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <br></br>
 
-                <form onSubmit={handleSubmit}>
-                  <Box>
-                    <TextField
-                      style={{ width: "400px" }}
-                      label="List Header (must be from one of above headers): "
-                      variant="filled"
-                      value={listHeader}
-                      onChange={handleHeader}
-                      multiline
-                      required
-                    />
-                  </Box>
-
-                  <Box>
-                    <TextField
-                      style={{ width: "400px" }}
-                      label="Description:"
-                      variant="filled"
-                      value={description}
-                      onChange={handleDescription}
-                      multiline
-                      required
-                    />
-                  </Box>
-                  <Box sx={{ display: "flex" }}>
-                    <Button
-                      style={{ width: "400px" }}
-                      type="submit"
-                      variant="contained"
-                      className="submit-button"
-                      color="primary"
-                    >
-                      Submit
-                    </Button>
-                  </Box>
-                </form>
-              </div>
-            </TableContainer>
-            <br></br>
-          </div>
           <div className="cognitive-footer">
             <p className="cognitive-notes">
               <b>Key: </b>COG_CONTRA means "Contraindications", COG_ACHEI means "Adverse Effects (AChEI)", COG_ACHEI_ME
               means "Adverse Effects (Memantine)", COG_BASELINE means "Baseline", COG_MONITOR means "monitoring"
             </p>
           </div>
-          <Footer />
-        </>
-      );
-    } else {
-      return (
-        <>
-          <Navigation />
-          <SearchBar placeholder="Search" data={Data} />
-          <div style={{ marginTop: "2rem", padding: "0 1rem" }}>
-            <Typography variant="h3" align="center" gutterBottom>
-              <div className="subtitle">Cognitive Enhancers Clinical Guide</div>
-            </Typography>
-
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 700 }} aria-label="customized table" id="antidepressantClinicalTable">
-                <TableHead>
-                  <TableRow>
-                    <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Contraindications</StyledTableCell>
-                    <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Adverse Effects (AChEI)</StyledTableCell>
-                    <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>
-                      Adverse Effects (Memantine)
-                    </StyledTableCell>
-                    <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Baseline</StyledTableCell>
-                    <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>Monitoring</StyledTableCell>
-                    <StyledTableCell style={{ backgroundColor: "#96d2b0" }}>How & When</StyledTableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {Array(
-                    Math.max(
-                      data.COG_ACHEI.length,
-                      data.COG_ACHEI_ME.length,
-                      data.COG_BASELINE.length,
-                      data.COG_CONTRA.length,
-                      data.COG_MONITOR.length
-                    )
-                  )
-                    .fill(null)
-                    .map((_, index) => (
-                      <StyledTableRow key={index}>
-                        <StyledTableCell>{data.COG_CONTRA[index] || ""}</StyledTableCell>
-                        <StyledTableCell>{data.COG_ACHEI[index] || ""}</StyledTableCell>
-                        <StyledTableCell>{data.COG_ACHEI_ME[index] || ""}</StyledTableCell>
-                        <StyledTableCell>{data.COG_BASELINE[index] || ""}</StyledTableCell>
-                        <StyledTableCell>{data.COG_MONITOR[index] || ""}</StyledTableCell>
-                        <StyledTableCell>{(data.COG_HOW_WHEN && data.COG_HOW_WHEN[index]) || ""}</StyledTableCell>
-                      </StyledTableRow>
-                    ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
-            <br></br>
-
-            <div className="cognitive-footer">
-              <p className="cognitive-notes">
-                <b>Key: </b>COG_CONTRA means "Contraindications", COG_ACHEI means "Adverse Effects (AChEI)",
-                COG_ACHEI_ME means "Adverse Effects (Memantine)", COG_BASELINE means "Baseline", COG_MONITOR means
-                "monitoring"
-              </p>
-            </div>
-          </div>
-          <Footer />
-        </>
-      );
-    }
+        </div>
+        <Footer />
+      </>
+    );
   }
 }
