2d0805264793bd2939c461e4573d9a086dbb9358
AidenPSM <sn410373@dal.ca>
2023-07-23 17:40:55 -0300

    User Register, Hash password update

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const authController = require('../controller/loginController');
 router.get('/login', authController.getLogin);
+router.post('/register', authController.postRegister);
 
 
 module.exports = router;
--- frontend/src/views/AdminPanel/Panel.js
+++ frontend/src/views/AdminPanel/Panel.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import createTheme from '@mui/material/styles/createTheme';
 import './Panel.css';
+import axios from "axios";
 import Navigation from '../Navigation/navigation';
 import FeedbackHandler from './feedback/AdminFeedbackHandler';
 import { ThemeProvider } from '@mui/material/styles';
@@ -14,6 +15,47 @@ const theme = createTheme({
 });
 
 function Panel() {
+    const [data, setData] = useState({});
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const [isRegisterVisible, setIsRegisterVisible] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const fetchData = () => {
+        axios
+            .get("http://localhost:8887/api/login")
+            .then((response) => {
+                setData(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    const handleRegisterClick = () => {
+        setIsRegisterVisible(!isRegisterVisible);
+    };
+
+    const handleRegisterSubmit = () => {
+        // Send register data to server
+        const userData = {
+            username: username,
+            password: password
+        };
+
+        axios.post("http://localhost:8887/api/register", userData)
+            .then((response) => {
+                console.log(response.data);
+                fetchData();
+                setIsRegisterVisible(false);
+            })
+            .catch((error) => {
+                console.error(error); 
+            });
+    };
 
     return (
         <ThemeProvider theme={theme}>
@@ -21,6 +63,30 @@ function Panel() {
             <div className="panel">
                 <h2>Admin Panel</h2>
                 <FeedbackHandler />
+            </div>
+            <div className="register">
+                <button className="btn-register" onClick={handleRegisterClick}>
+                    User Register
+                </button>
+                {isRegisterVisible && (
+                    <div className="register-area">
+                        <input className='register-input'
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <br />
+                        <input className='register-input'
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <br />
+                        <button className="btn-register" onClick={handleRegisterSubmit}>Register</button>
+                    </div>
+                )}
             </div>
         </ThemeProvider>
     )
