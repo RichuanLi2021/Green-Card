23bf69a5af8f45a15992af3d2de259cdec138a76
Syed Sameer Hasan <sy952280@dal.ca>
2023-07-23 22:31:45 -0300

    searchbar implemented in moodstablilizer category

--- frontend/src/views/Topics/MoodStabilizers/MoodStabilizers.js
+++ frontend/src/views/Topics/MoodStabilizers/MoodStabilizers.js
@@ -1,24 +1,32 @@
-import * as React from 'react';
-import Navigation from '../../Navigation/navigation';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import Typography from '@mui/material/Typography';
-import Data from "../../searchBar/Data.json";
-import SearchBar from "../../searchBar/searchBar";
+import * as React from "react";
+import Navigation from "../../Navigation/navigation";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Typography from "@mui/material/Typography";
+// import Data from "../../searchBar/Data.json";
+// import SearchBar from "../../searchBar/searchBar";
 import MoodStabilizersUpdate from "./moodStabilizersbackend";
-import './MoodStabilizers.css';
+import "./MoodStabilizers.css";
 
-import Footer from '../../Footer/Footer';
+import Footer from "../../Footer/Footer";
+import { useNavigate } from "react-router-dom";
+import Search from "../../Search/Search";
 
 export default function MoodStabilizers() {
   const [data, setData] = useState({});
+  const navigate = useNavigate();
+
+  const handleSearch = (searchTerm) => {
+    navigate(`/search/${searchTerm}`);
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = () => {
     axios
-      .get('http://localhost:8887/api/moodstabilizers')
+      .get("http://localhost:8887/api/moodstabilizers")
       .then((response) => {
         setData(response.data);
       })
@@ -28,8 +36,8 @@ export default function MoodStabilizers() {
   };
 
   const [selectedDrugs, setSelectedDrugs] = useState([]);
-  const [value, setValue] = useState('');
-  const admin = localStorage.getItem('admin');
+  const [value, setValue] = useState("");
+  const admin = localStorage.getItem("admin");
 
   //used to store value when an input is selected by user
   const store_value = (event) => {
@@ -44,7 +52,14 @@ export default function MoodStabilizers() {
         event.preventDefault();
         MoodStabilizersUpdate(event.target.name, event.target.id, event.target.value)
           .then((data) => {
-            alert('Updated Successfully Called! \nDrug:' + event.target.name + "\nColumn:" + event.target.id + "\nValue:"+ event.target.value);
+            alert(
+              "Updated Successfully Called! \nDrug:" +
+                event.target.name +
+                "\nColumn:" +
+                event.target.id +
+                "\nValue:" +
+                event.target.value
+            );
           })
           .catch((error) => {
             console.error(error);
@@ -57,7 +72,7 @@ export default function MoodStabilizers() {
       alert("You must be an administrator to edit");
     }
   };
-  
+
   const handleDrugClick = (dataObj) => {
     setSelectedDrugs((prevSelectedDrugs) => {
       const isSelected = prevSelectedDrugs.includes(dataObj);
@@ -68,21 +83,19 @@ export default function MoodStabilizers() {
       }
     });
   };
-  
+
   if (Object.keys(data).length > 0) {
     // Editable Fields
     if (admin) {
       return (
         <>
           <Navigation />
-          <SearchBar placeholder="Search" data={Data} />
-          <div style={{ marginTop: '1rem', padding: '0 1rem' }}>
+          <Search onSearch={handleSearch}></Search>
+          <div style={{ marginTop: "1rem", padding: "0 1rem" }}>
             <Typography variant="h4" align="center" gutterBottom>
-              <div className='subtitle-mood'>
-                Mood Stabilizers Guide
-              </div>
+              <div className="subtitle-mood">Mood Stabilizers Guide</div>
             </Typography>
-    
+
             <div className="grid-container">
               {Object.keys(data).map((id) => {
                 const dataObj = data[id];
@@ -91,102 +104,102 @@ export default function MoodStabilizers() {
                   <div className="grid-item" key={id}>
                     <button
                       onClick={() => handleDrugClick(dataObj)}
-                      className={`drug-button ${isDrugSelected ? 'active' : ''}`}
+                      className={`drug-button ${isDrugSelected ? "active" : ""}`}
                     >
                       {dataObj.Name}
                     </button>
-    
+
                     {isDrugSelected && (
                       <div className="box">
-
                         <div className="box-content">
-                        <strong>Half-life: </strong>
-                        <input
-                                id="`Half-life`"
-                                name={dataObj.Name}
-                                type="text"
-                                onFocus={store_value}
-                                onBlur={update_value}
-                                defaultValue={dataObj[`Half-life`]}
-                              />
+                          <strong>Half-life: </strong>
+                          <input
+                            id="`Half-life`"
+                            name={dataObj.Name}
+                            type="text"
+                            onFocus={store_value}
+                            onBlur={update_value}
+                            defaultValue={dataObj[`Half-life`]}
+                          />
                         </div>
 
                         <div className="box-content">
                           <strong>Dose (mg/day) Initial | Maint. | Max: </strong>
-                           <input
-                                id="`Dose (mg/day) Initial | Maint. | Max.`"
-                                name={dataObj.Name}
-                                type="text"
-                                onFocus={store_value}
-                                onBlur={update_value}
-                                defaultValue={dataObj[`Dose (mg/day) Initial | Maint. | Max.`]}
-                              />
+                          <input
+                            id="`Dose (mg/day) Initial | Maint. | Max.`"
+                            name={dataObj.Name}
+                            type="text"
+                            onFocus={store_value}
+                            onBlur={update_value}
+                            defaultValue={dataObj[`Dose (mg/day) Initial | Maint. | Max.`]}
+                          />
                         </div>
-    
+
                         <div className="box-content">
                           <strong>Frequency: </strong>
-                           <input
-                                id="`Frequency`"
-                                name={dataObj.Name}
-                                type="text"
-                                onFocus={store_value}
-                                onBlur={update_value}
-                                defaultValue={dataObj[`Frequency`]}
-                              />
+                          <input
+                            id="`Frequency`"
+                            name={dataObj.Name}
+                            type="text"
+                            onFocus={store_value}
+                            onBlur={update_value}
+                            defaultValue={dataObj[`Frequency`]}
+                          />
                         </div>
-                        
+
                         <div className="box-content">
                           <strong>mg/Form Supplied: </strong>
-                              <input
-                                id="`mg/Form Supplied`"
-                                name={dataObj.Name}
-                                type="text"
-                                onFocus={store_value}
-                                onBlur={update_value}
-                                defaultValue={dataObj[`mg/Form Supplied`]}
-                              />
+                          <input
+                            id="`mg/Form Supplied`"
+                            name={dataObj.Name}
+                            type="text"
+                            onFocus={store_value}
+                            onBlur={update_value}
+                            defaultValue={dataObj[`mg/Form Supplied`]}
+                          />
                         </div>
                         <div className="box-content">
                           <strong>Monitoring Level: </strong>
-                              <input
-                                id="`Monitoring Level`"
-                                name={dataObj.Name}
-                                type="text"
-                                onFocus={store_value}
-                                onBlur={update_value}
-                                defaultValue={dataObj[`Monitoring Level`]}
-                              />
+                          <input
+                            id="`Monitoring Level`"
+                            name={dataObj.Name}
+                            type="text"
+                            onFocus={store_value}
+                            onBlur={update_value}
+                            defaultValue={dataObj[`Monitoring Level`]}
+                          />
                         </div>
-
-                    </div>
+                      </div>
                     )}
                   </div>
                 );
               })}
             </div>
             <div className="mood-footer">
-            <p className='mood-notes'>
-            <b>Key: </b> †dosage determined by concomitant drugs used (see Lamictal monograph for details). er tab: slow release. <br /> <br />
-            <b>NOTES: </b> doses may not reflect manufacturers' recommendations, they are based on clinical literature and experience; most drugs in this category do not have a formal mood stabilizer indication. Levels may be useful for investigating toxicity and adherence, in addition to achieving a therapeutic dose. ^half-life of active metabolite. *due to risks for drug-drug interactions and adverse effects 
-            </p>
+              <p className="mood-notes">
+                <b>Key: </b> †dosage determined by concomitant drugs used (see Lamictal monograph for details). er tab:
+                slow release. <br /> <br />
+                <b>NOTES: </b> doses may not reflect manufacturers' recommendations, they are based on clinical
+                literature and experience; most drugs in this category do not have a formal mood stabilizer indication.
+                Levels may be useful for investigating toxicity and adherence, in addition to achieving a therapeutic
+                dose. ^half-life of active metabolite. *due to risks for drug-drug interactions and adverse effects
+              </p>
             </div>
           </div>
           <Footer />
         </>
       );
     }
-    
+
     // Non-Editable Fields
     else {
       return (
         <>
           <Navigation />
-          <SearchBar placeholder="Search" data={Data} />
-          <div style={{ marginTop: '1rem', padding: '0 1rem' }}>
+          <Search onSearch={handleSearch}></Search>
+          <div style={{ marginTop: "1rem", padding: "0 1rem" }}>
             <Typography variant="h4" align="center" gutterBottom>
-            <div className='subtitle-mood'>
-              Mood Stabilizers Guide
-              </div>
+              <div className="subtitle-mood">Mood Stabilizers Guide</div>
             </Typography>
 
             <div className="grid-container">
@@ -197,54 +210,55 @@ export default function MoodStabilizers() {
                   <div className="grid-item" key={id}>
                     <button
                       onClick={() => handleDrugClick(dataObj)}
-                      className={`drug-button ${isDrugSelected ? 'active' : ''}`}
+                      className={`drug-button ${isDrugSelected ? "active" : ""}`}
                     >
                       {dataObj.Name}
                     </button>
 
                     {isDrugSelected && (
-                    <div className="box">
-                      <div className="box-content">
-                        <strong>Half-life: </strong>
-                        <span>{dataObj[`Half-life`]}</span>
-                      </div>
-                      <div className="box-content">
-                        <strong>Dose (mg/day) Initial | Maint. | Max.: </strong>
-                        <span>{dataObj[`Dose (mg/day) Initial | Maint. | Max.`]}</span>
-                      </div>
-                      
-                      <div className="box-content">
-                        <strong>Frequency: </strong>
-                        <span>{dataObj[`Frequency`]}</span>
-                      </div>
+                      <div className="box">
+                        <div className="box-content">
+                          <strong>Half-life: </strong>
+                          <span>{dataObj[`Half-life`]}</span>
+                        </div>
+                        <div className="box-content">
+                          <strong>Dose (mg/day) Initial | Maint. | Max.: </strong>
+                          <span>{dataObj[`Dose (mg/day) Initial | Maint. | Max.`]}</span>
+                        </div>
 
-                      <div className="box-content">
-                        <strong>mg/Form Supplied: </strong>
-                        <span>{dataObj[`mg/Form Supplied`]}</span>
+                        <div className="box-content">
+                          <strong>Frequency: </strong>
+                          <span>{dataObj[`Frequency`]}</span>
+                        </div>
+
+                        <div className="box-content">
+                          <strong>mg/Form Supplied: </strong>
+                          <span>{dataObj[`mg/Form Supplied`]}</span>
+                        </div>
+                        <div className="box-content">
+                          <strong>Monitoring Level: </strong>
+                          <span>{dataObj[`Monitoring Level`]}</span>
+                        </div>
                       </div>
-                      <div className="box-content">
-                        <strong>Monitoring Level: </strong>
-                        <span>{dataObj[`Monitoring Level`]}</span>
-                      </div>
-                    </div>
-                  )}
-                </div>
-                  
+                    )}
+                  </div>
                 );
               })}
             </div>
             <div className="mood-footer">
-            <p className='mood-notes'>
-            <b>Key: </b> †dosage determined by concomitant drugs used (see Lamictal monograph for details). er tab: slow release. <br /> <br />
-            <b>NOTES: </b> doses may not reflect manufacturers' recommendations, they are based on clinical literature and experience; most drugs in this category do not have a formal mood stabilizer indication. Levels may be useful for investigating toxicity and adherence, in addition to achieving a therapeutic dose. ^half-life of active metabolite. *due to risks for drug-drug interactions and adverse effects 
-            </p>
+              <p className="mood-notes">
+                <b>Key: </b> †dosage determined by concomitant drugs used (see Lamictal monograph for details). er tab:
+                slow release. <br /> <br />
+                <b>NOTES: </b> doses may not reflect manufacturers' recommendations, they are based on clinical
+                literature and experience; most drugs in this category do not have a formal mood stabilizer indication.
+                Levels may be useful for investigating toxicity and adherence, in addition to achieving a therapeutic
+                dose. ^half-life of active metabolite. *due to risks for drug-drug interactions and adverse effects
+              </p>
             </div>
           </div>
           <Footer />
         </>
       );
     }
-  } 
+  }
 }
-
-
