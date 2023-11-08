5e63d62c78cedb467a938720ec0a751bc7b1d791
Nicholas MacNutt <nc495758@dal.ca>
2023-11-07 23:17:16 -0400

    (GPGC-402) (GPGC-405) Created 'auth_tokens' migration and model files and added 'jsonwebtoken' backend dependency,

---
+++ backend/migrations/20231108021527-create-auth-tokens.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, DataTypes) {
+    await queryInterface.createTable('auth_tokens', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      userID: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+      },
+      expiresAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    }).then(async () => {
+      await queryInterface.addConstraint('auth_tokens', {
+        type: 'FOREIGN KEY',
+        fields: ['userID'],
+        references: {
+          table: 'users',
+          field: 'id'
+        }
+      })
+    })
+  },
+  async down(queryInterface, DataTypes) {
+    await queryInterface.dropTable('auth_tokens');
+  }
+};
