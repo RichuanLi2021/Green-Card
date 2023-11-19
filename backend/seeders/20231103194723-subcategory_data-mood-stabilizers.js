79a9beba9f2e7421a88ca619372cb3459a38a8fb
Aryanfaizz <faizzaidan6546@gmail.com>
2023-11-18 23:01:48 -0400

    Added value seeds for Mood Stablilizers and Neuropsychiatric symptoms

--- backend/seeders/20231103194723-subcategory_data-mood-stabilizers.js
+++ backend/seeders/20231103194723-subcategory_data-mood-stabilizers.js
@@ -1,31 +1,53 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   async up (queryInterface, DataTypes) {
-    // await queryInterface.bulkInsert('subcategory_data', [
-      // Mood Stabilizers - Medication Table
-      // Uncomment these objects and bulkInsert() function, edit them for your specific headers, then copy and paste as you need.
-      // Use `npm run db:reset` to test your seed data.
-      // You can leave the subcategory title comment(s) alone, but delete these three comment lines.
-      // {
-      //   headerID: 1,
-      //   value: '',
-      //   info: '',
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // },
-      // {
-      //   headerID: 1,
-      //   value: '',
-      //   info: '',
-      //   createdAt: new Date(),
-      //   updatedAt: new Date()
-      // },
+    await queryInterface.bulkInsert('subcategory_data', [
+     
+      { uuid: uuidv4(), headerID: 61, value: 'lamotrigine (Lamictal)', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 62, value: '25h', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 63, value: '12.5-25 hs | 50--250† | 200-300', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 64, value: 'bid', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 65, value: '25,100,150/tab 2,5/chewtab', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 66, value: 'nil', createdAt: new Date(), updatedAt: new Date() },
 
-      // Mood Stabilizers - Adverse Effects and Safety
+      { uuid: uuidv4(), headerID: 61, value: 'lithium carbonate (Carbolith, Lithane)', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 62, value: '20-26h', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 63, value: '150 od-bid | use drug levels', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 64, value: 'hs-tid', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 65, value: '150,300,600/cap', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 66, value: '0.4-0.8 µmol/L', createdAt: new Date(), updatedAt: new Date() },
 
-    // ], {});
+      { uuid: uuidv4(), headerID: 61, value: 'lithium citrate (oral liquid)', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 62, value: '20-26h', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 63, value: '5 ml bid | use drug levels', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 64, value: 'od-tid', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 65, value: '8mmol/5ml (=300mg Li carb)', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 66, value: '"', createdAt: new Date(), updatedAt: new Date() },
+
+      { uuid: uuidv4(), headerID: 61, value: 'valproic acid (Depakene)', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 62, value: '6-16h', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 63, value: '125 od-bid | 1000-2000 | 60mg/kg', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 64, value: 'b-tid', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 65, value: '250,500/cap; 250/5ml liq', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 66, value: '350-700 µmol/L', createdAt: new Date(), updatedAt: new Date() },
+
+      { uuid: uuidv4(), headerID: 61, value: 'divalproex (Epival)', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 62, value: '6-16h', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 63, value: '125 od-bid | 1000-2000 | 60mg/kg', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 64, value: '"', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 65, value: '125,250,500/tab', createdAt: new Date(), updatedAt: new Date() },
+      { uuid: uuidv4(), headerID: 66, value: '"', createdAt: new Date(), updatedAt: new Date() },
+
+      { uuid: uuidv4(), headerID: 61, value: 'antipsychotics', createdAt: new Date(), updatedAt: new Date() },
+     
+      { uuid: uuidv4(), headerID: 61, value: 'carbamazepine (Tegretol)', createdAt: new Date(), updatedAt: new Date() },
+      
+      { uuid: uuidv4(), headerID: 61, value: 'oxcarbazepine (Trileptal)', createdAt: new Date(), updatedAt: new Date() },
+      
+    ], {});
   },
 
   async down (queryInterface, DataTypes) {
