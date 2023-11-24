0cb4667bba05e62c29da4db2a3c67313d434d463
Nithin Nedumparambil <nt923837@dal.ca>
2023-11-24 17:19:12 -0400

    (GPGC-480) Changed admin account password and removed user@gpgc.ca test account

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,51 +7,52 @@ const bcrypt = require('bcrypt')
 const env = require('../config/env')
 
 router.post('/login', async (req, res) => {
+  if (req.cookies['access-token']) return res.status(400).json({ errorMessage: 'Already logged in' })
+
   const { email, password } = req.body
+  // Sanitize and Validate
 
   try {
     const user = await User.findOne({
       where: { email: email }
     })
-    if (!user) res.status(400).json({ errorMessage: 'Incorrect email or password, or the account does not exist' })
+    if (!user) return res.status(400).json({ errorMessage: 'Incorrect email or password, or the account does not exist' })
+    if (!user.verified) return res.status(400).json({ errorMessage: 'Account has not been verified yet' })
 
-    const hashedPass = user.password
-    bcrypt.compare(password, hashedPass).then(async (match) => {
-      if (!match) {
-        res.status(400).json({ errorMessage: 'Incorrect email or password, or the account does not exist' })
-      } else {
-        await User.update(
-          {
-            lastLogin: new Date()
-          }, {
-            where: {id: user.id}
-          })
+    bcrypt.compare(password, user.password).then(async (match) => {
+      if (!match) return res.status(400).json({ errorMessage: 'Incorrect email or password, or the account does not exist' })
 
-        const role = await Role.findOne({
-          include: {
-            model: User_Role,
-            include: {
-              model: User,
-              where: {uuid: user.uuid}
-            }
-          }
+      await User.update(
+        {
+          lastLogin: new Date()
+        }, {
+          where: { id: user.id }
         })
-        const roleUUID = role.dataValues.uuid
 
-        const token = createToken(user, roleUUID)
-        return res
-          .status(200)
-          .cookie(
-            'access-token',
-            token,
-            {
-              httpOnly: true,
-              maxAge: env.JWT_LENGTH_MS,
-              sameSite: 'none',
-              secure: true
-            }
-          ).json({ message: 'Successfully logged in', token: token })
-      }
+      const role = await Role.findOne({
+        include: {
+          model: User_Role,
+          include: {
+            model: User,
+            where: { uuid: user.uuid }
+          }
+        }
+      })
+      const roleUUID = role.dataValues.uuid
+
+      const token = createToken(user, roleUUID)
+      return res
+        .status(200)
+        .cookie(
+          'access-token',
+          token,
+          {
+            httpOnly: true,
+            maxAge: env.JWT_LENGTH_MS,
+            sameSite: 'none',
+            secure: true
+          }
+        ).json({ message: 'Successfully logged in', token: token })
     })
   } catch (error) {
     return res.status(500).json({ error, errorMessage: 'Encountered unexpected error while logging in' })
@@ -60,29 +61,20 @@ router.post('/login', async (req, res) => {
 
 router.post('/logout', async (req, res) => {
   try {
-    if (!req.cookies['access-token']) {
-      return res
-        .status(400)
-        .json({ errorMessage: 'Not logged in' })
-    }
-
-    return res
-      .clearCookie('access-token')
-      .status(200)
-      .json({ message: 'Successfully logged out' })
+    if (!req.cookies['access-token']) return res.status(400).json({ errorMessage: 'Not logged in' })
+    return res.clearCookie('access-token').status(200).json({ message: 'Successfully logged out' })
   } catch (error) {
     return res.status(500).json({ error, errorMessage: 'Encountered unexpected error while logging out' })
   }
-
 })
 
 router.post('/register', async (req, res) => {
+  if (req.cookies['access-token']) return res.status(400).json({ errorMessage: 'Cannot register a new account while logged in' })
+
   const { discipline, email, password } = req.body
   // Sanitize and Validate
 
   try {
-    if (req.cookies['access-token']) return res.status(400).json({ errorMessage: 'Cannot register a new account while logged in' })
-
     bcrypt.hash(password, 12).then((hash) => {
         User.create({
           uuid: uuidv4(),
@@ -96,12 +88,12 @@ router.post('/register', async (req, res) => {
               userID: user.id,
               roleID: 2 // 'user' role
             })
-              .then(() => { res.status(201).json({ message: `Successfully registered account for ${user.email}`, user }) })
-              .catch((error) => { res.status(400).json({ error, errorMessage: error['errors'][0].message }) })
+              .then(() => { return res.status(201).json({ message: `Successfully registered account for ${user.email}`, user }) })
+              .catch((error) => { return res.status(400).json({ error, errorMessage: error['errors'][0].message }) })
             })
-          .catch((error) => { res.status(400).json({ error, errorMessage: error['errors'][0].message }) })
+          .catch((error) => { return res.status(400).json({ error, errorMessage: error['errors'][0].message }) })
     })
-      .catch((error) => { res.status(400).json({ error, errorMessage: 'Encountered unexpected error while registering account' }) })
+      .catch((error) => { return res.status(400).json({ error, errorMessage: 'Encountered unexpected error while registering account' }) })
   } catch (error) {
     return res.status(500).json({ error, errorMessage: 'Encountered unexpected error while registering account' })
   }
