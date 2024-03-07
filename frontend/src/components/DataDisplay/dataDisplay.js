6dc9d68cfea98349d8175fba5fd5d0b8f536db45
rabjot <rb588740@dal.ca>
2024-03-07 13:52:17 -0400

    Remove the multiple pages from the data display grid

--- frontend/src/components/DataDisplay/dataDisplay.js
+++ frontend/src/components/DataDisplay/dataDisplay.js
@@ -5,12 +5,10 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
 export default function StickyHeadTable({ drugName, subcategoryHeaders }) {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
 
   if (!subcategoryHeaders || subcategoryHeaders.length === 0) {
     return <div className="Liam"></div>;
@@ -35,55 +33,39 @@ export default function StickyHeadTable({ drugName, subcategoryHeaders }) {
     rows.push(row);
   }
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
+ 
 
   return (
     <Paper sx={{ width: '100%', maxWidth: '100%', overflow: 'hidden' }}>
-      <TableContainer>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {headers.map((header) => (
-                <TableCell
-                  key={header.id}
-                  align={header.align}
-                  style={{ minWidth: header.minWidth, fontSize: '16px'  }}
-                  
-                >
-                  {header.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, rowIndex) => (
-              <TableRow hover tabIndex={-1} key={rowIndex}>
-                {headers.map((header) => (
-                  <TableCell key={header.id} align={header.align} style={{ fontSize: '16px'  }}>
-                    {row[header.id]}
-                  </TableCell>
-                ))}
-              </TableRow>
+  <TableContainer>
+    <Table stickyHeader aria-label="sticky table">
+      <TableHead>
+        <TableRow>
+          {headers.map((header) => (
+            <TableCell
+              key={header.id}
+              align={header.align}
+              style={{ minWidth: header.minWidth, fontSize: '16px' }}
+            >
+              {header.label}
+            </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row, rowIndex) => (
+          <TableRow hover tabIndex={-1} key={rowIndex}>
+            {headers.map((header) => (
+              <TableCell key={header.id} align={header.align} style={{ fontSize: '16px' }}>
+                {row[header.id]}
+              </TableCell>
             ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+</Paper>
+
   );
 }
