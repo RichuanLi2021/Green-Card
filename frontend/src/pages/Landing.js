46c6c705de129676d372bf52315d4bd232a6d3c3
Sairaj Jadhav <sairajj8641@gmail.com>
2024-04-04 13:33:06 -0300

    Mobilised Landing Page - GPGC-582

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, Grid } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import logo from "../assets/images/icons/logo/white/WhiteShine256px.svg";
 import { useNavigate } from "react-router-dom";
@@ -16,55 +16,50 @@ const LandingPage = () => {
   };
 
   return (
-
-    <Box 
-    className="landing-content"
-    sx={{
-      width: "984px", 
-      height: "345px", 
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "space-between",
-      alignItems: "center",
-      margin: "auto", 
-      overflow: "auto", 
+    <Box
+      className="landing-content"
+      sx={{
+        maxWidth: "100%",
+        padding: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Box
+      <Avatar
         sx={{
-          textAlign: "center",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          p: 2, // Reduced padding here as well
-          height: "258px",
+          width: 110,
+          height: 110,
+          border: "2px solid #5a8e70",
+          bgcolor: "#96d2b0",
+          marginBottom: "1rem",
         }}
       >
-        <Avatar
-          sx={{
-            width: 110, // Slightly smaller avatar
-            height: 110, // Slightly smaller avatar
-            border: "2px solid #5a8e70",
-            bgcolor: "#96d2b0",
-            mb: 2, // Reduced margin
-          }}
-        >
-          <img src={logo} alt="GPGC Logo" className={"height-width-5rem"} style={{ height: '5.5rem', width: '5.5rem' }} />
-        </Avatar>
-        <Typography variant="h4" sx={{ mb: 1, px: 5, ...commonTextStyles }}>THE GREEN CARD</Typography>
-        <Typography variant="h6" sx={{ mb: 1, ...commonTextStyles }}>GERIATRIC PSYCHOTROPIC DRUG REFERENCE CARD</Typography>
-        <Typography variant="subtitle1" sx={{ mb: 1, ...commonTextStyles }}>
-          Kathleen Singh, MD, FRCPC; Terry Chisholm, MD, FRCPC; David Gardner,
-          PharmD, MSc
-        </Typography>
-        <Typography variant="subtitle2" sx={{ mb: 2, ...commonTextStyles }}>
-          Dept of Psychiatry, Dalhousie University, Halifax, CANADA
-        </Typography>
-        <div>
-          <Button variant="contained" onClick={handleRegister} sx={buttonStyle}>Register</Button>
-          <Button variant="contained" onClick={handleLogin} sx={buttonStyle}>Login</Button>
-        </div>
-      </Box>
+        <img
+          src={logo}
+          alt="GPGC Logo"
+          className={"height-width-5rem"}
+          style={{ height: "5.5rem", width: "5.5rem" }}
+        />
+      </Avatar>
+      <Typography variant="h4" gutterBottom align="center" sx={{ ...commonTextStyles }}>THE GREEN CARD</Typography>
+      <Typography variant="h6" gutterBottom align="center" sx={{ ...commonTextStyles }}>GERIATRIC PSYCHOTROPIC DRUG REFERENCE CARD</Typography>
+      <Typography variant="subtitle1" gutterBottom align="center" sx={{ ...commonTextStyles }}>
+        Kathleen Singh, MD, FRCPC; Terry Chisholm, MD, FRCPC; David Gardner,
+        PharmD, MSc
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom align="center" sx={{ ...commonTextStyles }}>
+        Dept of Psychiatry, Dalhousie University, Halifax, CANADA
+      </Typography>
+      <Grid container spacing={2} justifyContent="center">
+        <Grid item>
+          <Button variant="contained" fullWidth onClick={handleRegister} sx={buttonStyle}>Register</Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" fullWidth onClick={handleLogin} sx={buttonStyle}>Login</Button>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
@@ -74,11 +69,11 @@ const commonTextStyles = {
   backgroundColor: "#355944",
   WebkitBackgroundClip: "text",
   WebkitTextFillColor: "transparent",
+  marginBottom: "0.5rem",
 };
 
 const buttonStyle = {
   fontWeight: "bold",
-  marginRight: "8px",
   backgroundColor: "#8ab89d",
   color: "#385143",
   '&:hover': {
