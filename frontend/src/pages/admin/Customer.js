89f072debdc81609b70c6e25fd13f177ea0c61f4
Christopher Edgett <ch858173@dal.ca>
2024-06-22 16:32:15 -0300

    GPGC-644 Added sort/reset button to customers table with dropdown

--- frontend/src/pages/admin/Customer.js
+++ frontend/src/pages/admin/Customer.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField } from '@mui/material';
+import { Box, Typography, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, Button, ButtonGroup, Menu, MenuItem } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
 import Config from "../../config/config";
@@ -16,22 +16,51 @@ const theme = createTheme({
 const Customer = () => {
   const [customersList, setCustomersList] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [anchorE1, setAnchorE1] = useState(null); //For handling dropdown menu
 
+  const fetchCustomers = async () => {
+    try {
+      const response = await axios.get(`${Config.API_URL}/api/users`, { withCredentials: true });
+      setCustomersList(response.data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
   useEffect(() => {
-    const fetchCustomers = async () => {
-      try {
-        const response = await axios.get(`${Config.API_URL}/api/users`, { withCredentials: true });
-        setCustomersList(response.data);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
-    };
-
     fetchCustomers();
   }, []);
 
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
+  };
+
+  //Handlers
+  const handleSortByDiscipline = () => {
+    const sortedList = [...customersList].sort((a, b) => a.discipline.localeCompare(b.discipline));
+    setCustomersList(sortedList);
+    handleCloseMenu();
+  };
+
+  const handleSortByTitle = () => {
+    const sortedList = [...customersList].sort((a, b) => a.title.localeCompare(b.title));
+    setCustomersList(sortedList);
+    handleCloseMenu();
+  };
+
+  const handleReset = () => {
+    // Logic to reset any filters or sorting
+    setSearchQuery('');
+    fetchCustomers();
+    setAnchorE1();
+    handleCloseMenu();
+  };
+
+  const handleMenuClick = (event) => {
+    setAnchorE1(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorE1(null);
   };
 
   const filteredCustomersList = customersList.filter((customer) =>
@@ -50,9 +79,21 @@ const Customer = () => {
           size="small"
           value={searchQuery}
           onChange={handleSearchChange}
-          fullWidth
-          style={{ marginBottom: "1rem" }}
+          style={{width:"20%", marginBottom: "1rem" }}
         />
+        <ButtonGroup variant="contained" aria-label="sort and reset button">
+          <Button onClick={handleMenuClick}>Sort</Button>
+          <Menu
+            anchorE1={anchorE1}
+            open={Boolean(anchorE1)}
+            onClose={handleCloseMenu}
+            anchorOrigin={{vertical: 'center', horizontal: 'center'}}
+            >
+              <MenuItem onClick={handleSortByDiscipline}>By Discipline</MenuItem>
+              <MenuItem onClick={handleSortByTitle}>By Title</MenuItem>
+              </Menu>
+              <Button onClick={handleReset}>Reset</Button>
+        </ButtonGroup>
         <div style={{ overflowX: "auto" }}>
           <TableContainer component={Paper}>
             <Table aria-label="customer table">
